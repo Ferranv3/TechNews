1300c05d9c58bbf1dc402e5ec4d651e553406c75
Ferranv3 <ferranhpv3@gmail.com>
2023-06-25 11:55:20 +0200

    Scrapping to chapuzasinformatico done

--- api/scrape.js
+++ api/scrape.js
@@ -1,13 +1,13 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+import axios from 'axios';
+import cheerio from 'cheerio';
 
-module.exports = async (req, res) => {
+export default async function scrapeWebsite() {
   try {
     const url = 'https://elchapuzasinformatico.com/';
     const response = await axios(url);
     const html = response.data;
     const $ = cheerio.load(html);
-    const links = $('article.blog-item');
+    const articles = $('article.blog-item');
 
     const scrapedData = [];
     articles.each((index, element) => {
@@ -15,13 +15,14 @@ module.exports = async (req, res) => {
       const title = $(element).find('h2.entry-title a').text();
       const description = $(element).find('p.post-excerpt').text();
       const href = link.attr('href');
-      const style = link.attr('href');
+      //const img = $(element).find('a.rocket-lazyload img').attr('src');
+      //const style = 'background-image: url("https://elchapuzasinformatico.com/wp-content/uploads/2023/06/OceanGate-Titan-controlado-por-Logitech-F710-378x150.jpg")';
       
-      scrapedData.push({ title, description, href, style });
+      scrapedData.push({ title, description, href });
     });
 
-    res.status(200).json(scrapedData);
+    return scrapedData;
   } catch (error) {
-    res.status(500).json({ error: 'Error scraping data' });
+    return [];
   }
 };
---
+++ src/components/Scraping.jsx
@@ -0,0 +1,13 @@
+/*import { scrapeNews } from '../../api/scrape.js';
+
+export default {
+  async load() {
+    const updateNews = async () => {
+      const news = await scrapeNews();
+      this.data.news = news;
+    };
+
+    await updateNews();
+    setInterval(updateNews, 60000);
+  }
+};*/
