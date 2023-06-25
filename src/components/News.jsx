80f12b92bf9756748023563bcdeaa5bd421ecb43
Ferranv3 <ferranhpv3@gmail.com>
2023-06-25 16:33:50 +0200

    React component to render news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,25 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
 import styles from './news.module.css';
 
-const News = ({ news }) => {
+const News = () => {
+    const [source, setSource] = useState('elchapuzas');
+    const [news, setNews] = useState([]);
 
-    const [filteredNews, setFilteredNews] = useState([]);
+    const sources = [
+        { value: 'elchapuzas', label: 'ElChapuzasInformatico' },
+        { value: 'profesionalreview', label: 'ProfesionalReview' },
+        { value: 'hardzone', label: 'HardZone' }
+    ];
 
-    const handleClick = (source) => {
-        setFilteredNews(news?.filter(newInfo => newInfo.source === source));
-    };
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`/api/scrape?source=${source}`);
+                setNews(response.data);
+            } catch (error) {
+                console.error("Error al recuperar las noticias: ", error);
+            }
+        }
+        fetchData();
+    }, [source]);
+
+    const handleSourceChange = async (e) => {
+        const newSource = e.target.value;
+        setSource(newSource);
+      };
 
     return (
         <>
-            <section class={styles.searchButtonsContainer}>
-            <button onClick={() => handleClick('elchapuzas')}>ElChapuzasInformatico</button>
-                <button>ProfesionalReview</button>
-                <button>HardZone</button>
-            </section>
+            <select value={source} onChange={handleSourceChange} className={styles.searchButtonsContainer}>
+                {sources.map(source => (
+                    <option key={source.value} value={source.value}>{source.label}</option>
+                ))}
+            </select>
             <ul>
-                {filteredNews.map(item => (
+                {news?.map(item => (
                 <li key={item.href}>
-                    <a href={item.href} class={styles.textArticle}>
+                    <a href={item.href} className={styles.textArticle}>
                     <h2>{item.title}</h2>
                     <p>{item.description}</p>
                     </a>
